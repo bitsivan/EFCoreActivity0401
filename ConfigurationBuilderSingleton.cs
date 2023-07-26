3f3ee705125f0a825a919a7cb0da78666fdbdb16
ivan montejo <sergio.montejo@improving.com>
2023-07-25 18:01:02 -0600

    first commit

---
+++ ConfigurationBuilderSingleton.cs
@@ -0,0 +1,44 @@
+using Microsoft.Extensions.Configuration;
+
+namespace EFCore_Activity0302
+{
+    public sealed class ConfigurationBuilderSingleton
+    {
+        private static ConfigurationBuilderSingleton _instance = null;
+        private static readonly object instanceLock = new object();
+        private static IConfigurationRoot _configuration;
+        public ConfigurationBuilderSingleton()
+        {
+            var builder = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: true,
+                    reloadOnChange: true);
+                    
+            _configuration = builder.Build();
+        }
+
+        public static ConfigurationBuilderSingleton Instance
+        {
+            get
+            {
+                lock (instanceLock) 
+                { 
+                    if (_instance == null)
+                    {
+                        _instance = new ConfigurationBuilderSingleton();
+                    }
+                    return _instance;
+                }
+            }
+        }
+
+        public static IConfigurationRoot ConfigurationRoot
+        {
+            get
+            {
+                if(_configuration == null) { var x = Instance; }
+                return _configuration;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using EFCore_DBLibrary;
+using InventoryModels;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace EFCore_Activity0302;
+
+public class Program
+{
+    private static IConfigurationRoot _configuration;
+    private static DbContextOptionsBuilder<InventoryDBContext>
+    _optionsBuilder;
+    static void Main(string[] args)
+    {
+        BuildOptions();
+        EnsureItems();
+        ListInventory();
+    }
+    static void BuildOptions()
+    {
+        _configuration = ConfigurationBuilderSingleton.ConfigurationRoot;
+        _optionsBuilder = new DbContextOptionsBuilder<InventoryDBContext>();
+        _optionsBuilder.UseSqlServer(_configuration.GetConnectionString(
+       "InventoryManager"));
+    }
+
+    static void EnsureItems()
+    {
+        EnsureItem("Batman Begins");
+        EnsureItem("Inception");
+        EnsureItem("Star Wars");
+        EnsureItem("Sonds of Freedom");
+        EnsureItem("Top Gun");
+    }
+
+    private static void EnsureItem(string name)
+    {
+        using (var db = new InventoryDBContext(_optionsBuilder.Options))
+        {
+            var existingItems = db.Items.FirstOrDefault(x => x.Name.ToLower() == name.ToLower());
+            if (existingItems == null)
+            {
+                var item = new Item() { Name = name };
+                db.Items.Add(item);
+                db.SaveChanges();
+            }
+        }
+    }
+
+    private static void ListInventory()
+    {
+        using (var db = new InventoryDBContext(_optionsBuilder.Options))
+        {
+            var items = db.Items.OrderBy(x => x.Name).ToList();
+            items.ForEach(x => Console.WriteLine($"New Item: {x.Name}"));
+        }
+    }
+}
